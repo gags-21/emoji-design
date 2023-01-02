5ec95463926dcd41b18c87a6296bb99896d517dc
Gagan Bhirangi <gagan.bhirangi123@gmail.com>
2023-01-02 12:42:12 +0530

    Initial Commit

---
+++ EmojiDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiDesign
+//
+//  Created by Gagan Bhirangi on 02/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiDesign/EmojiDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDesignApp.swift
+//  EmojiDesign
+//
+//  Created by Gagan Bhirangi on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
